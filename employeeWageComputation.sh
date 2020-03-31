1c21ad5c132cfcdad95e12c260f2fb37b1ad1df6
seema1611 <seemarajpure16@gmail.com>
2020-03-31 17:39:20 +0530

    ADDED: Write a function for calculated work hours

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-#Program to findout Employee Wages till hours and days is reached For A Month
+#Program to findout Employee Wages till hours and days is reached For A Month using function
 #Constants
 EMPLOYEE_RATE_PER_HOUR=20
 IS_FULL_TIME=2
@@ -12,22 +12,26 @@ WORKING_HOURS_PER_MONTH=100
 totalWorkingDays=0
 totalWorkingHours=0
 
+function getEmployeeHours() {
+	case $1 in
+		$IS_FULL_TIME)
+			empHours=8
+         ;;
+      $IS_PART_TIME)
+         empHours=4
+         ;;
+		*)
+         empHours=0
+         ;;
+	esac
+	echo $empHours
+}
+
 while [[ $totalWorkingDays -lt $WORKING_DAYS_PER_MONTH && $totalWorkingHours -lt $WORKING_HOURS_PER_MONTH ]]
 do
 	((totalWorkingDays++))
-	randomCheck=$((RANDOM % 3 ))
-	case $randomCheck in
-		$IS_FULL_TIME)
-			empHours=8
-			;;
-		$IS_PART_TIME)
-			empHours=4
-			;;
-		*)
-			empHours=0
-			;;
-	esac
-	totalWorkingHours=$(($totalWorkingHours + $empHours ))
+	employeeHours="$( getEmployeeHours $((RANDOM%3)) )"
+	totalWorkingHours=$(($totalWorkingHours + $employeeHours ))
 done
 totalSalary=$(($totalWorkingHours * $EMPLOYEE_RATE_PER_HOUR ))
 echo $totalSalary
