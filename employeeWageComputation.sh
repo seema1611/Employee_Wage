0b4cc819be2ded06e4b3e4e1c5ec5ce258c19a90
seema1611 <seemarajpure16@gmail.com>
2020-03-31 16:49:54 +0530

    ADDED: Calculate Employee wage Using switch Case

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-#Program to findout daily Employee wage with partTime And FullTime
+#Program to findout daily Employee wage using switch case
 #Constants
 EMPLOYEE_RATE_PER_HOUR=20
 IS_FULL_TIME=2
@@ -8,14 +8,16 @@ IS_PART_TIME=1
 
 isPresent=1
 randomCheck=$((RANDOM % 3 ))
-if [ $IS_FULL_TIME -eq $randomCheck ]
-then
-	empHours=8
-elif [ $IS_PART_TIME -eq $randomCheck ]
-then
-	empHours=4
-else
-	empHours=0
-fi
+case $randomCheck in
+		$IS_FULL_TIME)
+			empHours=8
+			;;
+		$IS_PART_TIME)
+			empHours=4
+			;;
+		*)
+			empHours=0
+			;;
+esac
 dailyEmpWage=$(($empHours * $EMPLOYEE_RATE_PER_HOUR ))
 echo $dailyEmpWage
