ba2d7d8b91dabd427482a8b4afa272d39acbd3f7
seema1611 <seemarajpure16@gmail.com>
2020-03-31 18:28:59 +0530

    ADDED: Store the daily wage of employee along with the total wage

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,37 +1,55 @@
-#!/bin/bash -x
+#! /bin/bash -x
 
-#Program to findout Employee Wages till hours and days is reached For A Month using function
-#Constants
-EMPLOYEE_RATE_PER_HOUR=20
-IS_FULL_TIME=2
+#Program to findout and store workingHours and total salary of Employee
+#Declaration of Array
+declare -a empDailyWage
+
+#CONSTANTS FOR THE PROGRAM
 IS_PART_TIME=1
+IS_FULL_TIME=2
+EMPLOYEE_RATE_PER_HOUR=20
 WORKING_DAYS_PER_MONTH=20
 WORKING_HOURS_PER_MONTH=100
 
-#Variables
+#Variable
 totalWorkingDays=0
 totalWorkingHours=0
 
-function getEmployeeHours() {
-	case $1 in
-		$IS_FULL_TIME)
-			empHours=8
+#Function to find Employee working hours
+function getWorkingHours() {
+   case $1 in
+      $IS_FULL_TIME)
+         empHours=8
          ;;
       $IS_PART_TIME)
          empHours=4
          ;;
-		*)
+       *)
          empHours=0
-         ;;
-	esac
-	echo $empHours
+          ;;
+   esac
+   echo $empHours
 }
 
-while [[ $totalWorkingDays -lt $WORKING_DAYS_PER_MONTH && $totalWorkingHours -lt $WORKING_HOURS_PER_MONTH ]]
-do
-	((totalWorkingDays++))
-	employeeHours="$( getEmployeeHours $((RANDOM%3)) )"
-	totalWorkingHours=$(($totalWorkingHours + $employeeHours ))
-done
-totalSalary=$(($totalWorkingHours * $EMPLOYEE_RATE_PER_HOUR ))
-echo $totalSalary
+#Function to calculate daily wage
+function getDailyWage() {
+	local workHours=$1
+	wageOfDay=$(($workHours * $EMPLOYEE_RATE_PER_HOUR))
+	echo $wageOfDay
+}
+
+#Function to store and calculate daily and total wage
+function employeeWageComputation () {
+	while [[ $totalWorkingHours -lt $WORKING_HOURS_PER_MONTH && $totalWorkingDays -lt $WORKING_DAYS_PER_MONTH ]]
+	do
+   	(( totalWorkingDays++))
+    	workHours="$( getWorkingHours $(( RANDOM%3)) )"
+    	totalWorkingHours=$(($totalWorkingHours+$workHours)) 										  #Calculate here total Employee hours
+	 	empDailyWage[$totalWorkingDays]="$( getDailyWage $workHours )"
+	done
+totalSalary=$(($totalWorkingHours * $EMPLOYEE_RATE_PER_HOUR))									  #Calculate total salary here
+echo "Daily Wage: " ${empDailyWage[@]}
+}
+
+#First function of the program to check hours and days reached 100 and 20 resp
+employeeWageComputation
