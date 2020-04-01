2fcfcbcecea20c6b2d527964d5d4ff3f106499af
seema1611 <seemarajpure16@gmail.com>
2020-04-01 17:55:19 +0530

    MODIFIED: Calculate daily wage till days reached 20

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -17,22 +17,24 @@ totalWorkingHours=0
 
 #Function to find Employee working hours
 function getWorkingHours() {
+
    case $1 in
       $IS_FULL_TIME)
-         empHours=8
-         ;;
+         	empHours=8
+         	;;
       $IS_PART_TIME)
-         empHours=4
-         ;;
+				empHours=4
+         	;;
        *)
-         empHours=0
-          ;;
+         	empHours=0
+          	;;
    esac
    echo $empHours
 }
 
 #Function to calculate daily wage of Employee
 function getDailyWage() {
+
 	local workHours=$1
 	wageOfDay=$(($workHours * $EMPLOYEE_RATE_PER_HOUR))
 	echo $wageOfDay
@@ -40,6 +42,7 @@ function getDailyWage() {
 
 #Function to store and calculate daily and total wage
 function employeeWageComputation () {
+
 	while [[ $totalWorkingHours -lt $WORKING_HOURS_PER_MONTH && $totalWorkingDays -lt $WORKING_DAYS_PER_MONTH ]]
 	do
    		(( totalWorkingDays++))
@@ -47,6 +50,7 @@ function employeeWageComputation () {
 		totalWorkingHours=$(($totalWorkingHours+$workHours))
 		empDailyWage[$totalWorkingDays]="$( getDailyWage $workHours )"
 	done
+
 totalSalary=$(($totalWorkingHours * $EMPLOYEE_RATE_PER_HOUR))
 echo "Day:           " ${!empDailyWage[@]}
 echo "Daily Wage:    " ${empDailyWage[@]}
