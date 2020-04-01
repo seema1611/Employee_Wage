66448209d39c8a4da37f6a7f1360a7457afdf4dd
seema1611 <seemarajpure16@gmail.com>
2020-04-01 15:17:26 +0530

    MODIFIED: Write function for calculating workhours

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -x
 
-#Program to findout total salary of Employee till reache 100 hours and 20 days 
+#Program to findout and store workingHours and total salary of Employee
 #Declaration of Array
 declare -a empDailyWage
 
@@ -31,7 +31,7 @@ function getWorkingHours() {
    echo $empHours
 }
 
-#Function to calculate daily wage of Employee
+#Function to calculate daily wage
 function getDailyWage() {
 	local workHours=$1
 	wageOfDay=$(($workHours * $EMPLOYEE_RATE_PER_HOUR))
@@ -42,15 +42,13 @@ function getDailyWage() {
 function employeeWageComputation () {
 	while [[ $totalWorkingHours -lt $WORKING_HOURS_PER_MONTH && $totalWorkingDays -lt $WORKING_DAYS_PER_MONTH ]]
 	do
-   	(( totalWorkingDays++))
-    	workHours="$( getWorkingHours $(( RANDOM%3)) )"
-	totalWorkingHours=$(($totalWorkingHours+$workHours))
-	empDailyWage[$totalWorkingDays]="$( getDailyWage $workHours )"
+   		(( totalWorkingDays++))
+    		workHours="$( getWorkingHours $(( RANDOM%3)) )"
+    		totalWorkingHours=$(($totalWorkingHours+$workHours)) 										 rs
+			empDailyWage[$totalWorkingDays]="$( getDailyWage $workHours )"
 	done
 totalSalary=$(($totalWorkingHours * $EMPLOYEE_RATE_PER_HOUR))
-echo "Day:           " ${!empDailyWage[@]}
-echo "Daily Wage:    " ${empDailyWage[@]}
-echo "Total salary:  " $totalSalary
+echo "Daily Wage: " ${empDailyWage[@]}
 }
 
 #First function of the program to check hours and days reached 100 and 20 resp
